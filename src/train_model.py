ebeb7e3948582cf4de535a2b96c57341d768cbc7
saijithendr <jithenkumar1@gmail.com>
2022-12-04 23:03:31 +0100

    training initialized

--- src/train_model.py
+++ src/train_model.py
@@ -3,11 +3,14 @@ import torch
 import torchvision
 import torch.nn as nn
 import torchvision.transforms as T
+from utils import CNN
+
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
 def load_data(train_dir: str, test_dir: str, batch_size: int):
     
     transform = T.Compose(
-        [T.Resize(150, 200), T.ToTensor()]
+        [T.Resize((150, 150), T.InterpolationMode.BILINEAR), T.ToTensor()]
     )
     
     train_dataset = torchvision.datasets.ImageFolder(
@@ -30,8 +33,8 @@ test_dir = os.path.join('data/cats_dogs/PetImages', 'test')
 
 # Hyperparameters
 batch_size = 32
-learning_rate = 0.001
-num_epochs = 20
+learning_rate = 0.01
+num_epochs = 10
 
 
 
@@ -39,3 +42,30 @@ train_data, test_data = load_data(
     train_dir=train_dir, test_dir=test_dir, batch_size=batch_size
 )
 
+# Training 
+model = CNN().to(device)
+
+optimizer = torch.optim.Adam(
+    params=model.parameters(), lr=learning_rate )
+
+criterion = nn.CrossEntropyLoss()
+
+n_total_steps = len(train_data)
+
+#Loop 
+for epoch in range(num_epochs):
+    for id, (img, label) in enumerate(train_data):
+        
+        img = img.to(device)
+        label = label.to(device)
+        
+        # forward pass
+        output = model(img)
+        loss = criterion(output, label)
+        
+        #Backward and loss
+        optimizer.zero_grad()
+        loss.backward()
+        
+        if (id+1) % 100 == 0:
+            print(f'epoch {epoch+1} / {num_epochs}, step {id+1}/{n_total_steps}, loss = {loss.item():.4f}')
